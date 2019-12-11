67ff70e80d910d563888995c565395533d7b355a
Otto <otto@vonsperling.com.br>
2019-12-10 23:33:04 -0300

    LOADS data into a multi-level indexed Pandas DataFrame object

--- src/eda.py
+++ src/eda.py
@@ -1,16 +1,86 @@
 import streamlit as st
 import pandas as pd
 
-# Reuse this data across runs!
-read_and_cache_csv = st.cache(pd.read_csv)
 
-# BUCKET = 'data/raw/breast-cancer-wisconsin.data'
-# data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=100)
-desired_label = st.selectbox('Filter to:', ['car', 'truck'])
-st.write(data[data.label == desired_label])
-
-class Explorer:
+class BCW_Explorer:
     '''
     Class to explore the dataset
     '''
-    
+    def __init__(self, nrows=100):
+        '''
+        '''
+        # @st.cache
+        def load_data() -> None:
+            # defines header for the dataframe following the documentation
+            # header =\
+            #     ['id', 'label'] +\
+            #     [
+            #         '{0}_{1}'.format(attribute, summary)
+            #         for summary in ['mean', 'std', 'mean_3max']
+            #         for attribute in [
+            #             'radius', 'texture',
+            #             'circumference', 'area',
+            #             'smoothness', 'density',
+            #             'concavity_intensity', 'concavity_count',
+            #             'symmetry', 'fractal_dimension'
+            #         ]
+            #     ]
+
+            try:
+                data = pd.read_csv(
+                    'data/raw/wdbc.data',
+                    header=None,
+                    # names=pd.np.array(header),
+                    nrows=nrows
+                )
+
+            except Exception as e:
+                print('Error while loading data: {0}'.format(e))
+
+            # drops unnecessary id column
+            data = data.drop(0, axis=1)
+
+            # transform labels into binary
+            labels = data[1].map({'M': 1, 'B': 0}).astype('Int8')
+
+            # enforcing multi-level index
+            summaries = ['mean', 'std', 'mean_max3']
+            multilevel =\
+                pd.concat(
+                    [
+                        data[cols]
+                        .rename(
+                            columns={
+                                x: attribute
+                                for x, attribute
+                                in zip(
+                                    cols,
+                                    [
+                                        'radius', 'texture',
+                                        'circumference', 'area',
+                                        'smoothness', 'density',
+                                        'concavity_intensity',
+                                        'concavity_count',
+                                        'symmetry', 'fractal_dimension'
+                                    ]
+                                )
+                            }
+                        )
+                        for cols in pd.np.array_split(
+                            range(2, 32), len(summaries)
+                        )
+                    ],
+                    axis=1,
+                    keys=summaries
+                )
+
+            multilevel['label'] = labels
+            return multilevel
+
+        self.data = load_data()
+
+
+# st.title('Elogroup - Data Scientist Applicant')
+# st.header('Case: Breast Cancer Wisconsin EDA')
+# eda = BCW_Explorer()
+# st.write()
